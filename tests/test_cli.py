7c2bd887cd7407d2b6fc399afc296f671b06da81
Ryan Finnie <ryan@finnie.org>
2017-07-26 16:48:26 -0700

    Further unittest CLI improvements

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,22 +10,24 @@ from twoping import monotonic_clock
 
 
 @unittest.skipUnless(hasattr(os, 'fork'), 'CLI tests require os.fork()')
-class TestCLI(unittest.TestCase):
+class BaseTestCLI(unittest.TestCase):
     bind_address = '127.0.0.1'
     port = None
-    settle_time = 1
+    settle_time = 3
     child_pid = 0
     twoing_binary = '/usr/bin/2ping'
+    listener_opts = []
 
     @classmethod
     def setUpClass(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self.twoping_binary = os.path.join(os.path.split(dir_path)[0], '2ping')
+        (self.child_pid, self.port) = self.fork_listener(self, self.listener_opts)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         if self.child_pid:
             os.kill(self.child_pid, signal.SIGINT)
-        self.child_pid = 0
 
     def fork_listener(self, extra_opts=None):
         if self.port:
@@ -36,6 +38,7 @@ class TestCLI(unittest.TestCase):
             '2ping',
             '--listen',
             '--quiet',
+            '--no-3way',
             '--interface-address={}'.format(self.bind_address),
             '--port={}'.format(port),
         ]
@@ -55,21 +58,26 @@ class TestCLI(unittest.TestCase):
     def run_listener_client(self, client_opts, listener_opts=None):
         if listener_opts is None:
             listener_opts = []
-        (self.child_pid, port) = self.fork_listener(listener_opts)
         client_base_opts = [
             self.twoping_binary,
             self.bind_address,
-            '--port={}'.format(port),
+            '--port={}'.format(self.port),
             '--debug',
-            '--nagios=1000,1%,1000,1%',
+            '--nagios=1000,5%,1000,5%',
         ]
+        if not (('--adaptive' in client_opts) or ('--flood' in client_opts)):
+            client_base_opts.append('--count=1')
+        if not ('--count=1' in client_opts):
+            client_base_opts.append('--interval=5')
         try:
             subprocess.check_output(client_base_opts + client_opts)
         except subprocess.CalledProcessError as e:
             self.fail(e.output)
 
+
+class TestCLIStandard(BaseTestCLI):
     def test_notice(self):
-        self.run_listener_client(['--count=1', '--notice=Notice text'])
+        self.run_listener_client(['--notice=Notice text'])
 
     @unittest.skipUnless(
         (
@@ -78,17 +86,17 @@ class TestCLI(unittest.TestCase):
         ), 'UTF-8 environment required'
     )
     def test_notice_utf8(self):
-        self.run_listener_client(['--count=1', '--notice=UTF-8 \u2603'])
+        self.run_listener_client(['--notice=UTF-8 \u2603'])
 
     def test_random(self):
-        self.run_listener_client(['--count=1', '--send-random=32'])
+        self.run_listener_client(['--send-random=32'])
 
     def test_time(self):
-        self.run_listener_client(['--count=1', '--send-time'])
+        self.run_listener_client(['--send-time'])
 
     @unittest.skipUnless(monotonic_clock.get_clock_info('clock').monotonic, 'Monotonic clock required')
     def test_monotonic_clock(self):
-        self.run_listener_client(['--count=1', '--send-monotonic-clock'])
+        self.run_listener_client(['--send-monotonic-clock'])
 
     def test_adaptive(self):
         self.run_listener_client(['--adaptive', '--deadline=3'])
@@ -96,35 +104,40 @@ class TestCLI(unittest.TestCase):
     def test_flood(self):
         self.run_listener_client(['--flood', '--deadline=3'])
 
-    def test_hmac_md5(self):
-        self.run_listener_client(
-            ['--nagios=1000,5%,1000,5%', '--count=1', '--auth-digest=hmac-md5', '--auth=rBgRpBfRbF4DkwFQXncz'],
-            ['--auth-digest=hmac-md5', '--auth=rBgRpBfRbF4DkwFQXncz']
-        )
 
-    def test_hmac_sha1(self):
-        self.run_listener_client(
-            ['--nagios=1000,5%,1000,5%', '--count=1', '--auth-digest=hmac-sha1', '--auth=qnzTCJHnZXdrxRZ8JjQw'],
-            ['--auth-digest=hmac-sha1', '--auth=qnzTCJHnZXdrxRZ8JjQw']
-        )
+class TestCLIHMACMD5(BaseTestCLI):
+    listener_opts = ['--auth-digest=hmac-md5', '--auth=rBgRpBfRbF4DkwFQXncz']
 
-    def test_hmac_sha256(self):
-        self.run_listener_client(
-            ['--nagios=1000,5%,1000,5%', '--count=1', '--auth-digest=hmac-sha256', '--auth=cc8G2Ssbq4WZRq7H7d5L'],
-            ['--auth-digest=hmac-sha256', '--auth=cc8G2Ssbq4WZRq7H7d5L']
-        )
+    def test_hmac(self):
+        self.run_listener_client(self.listener_opts)
 
-    def test_hmac_sha512(self):
-        self.run_listener_client(
-            ['--nagios=1000,5%,1000,5%', '--count=1', '--auth-digest=hmac-sha512', '--auth=sjk3kqzcSV3XfHJWNstn'],
-            ['--auth-digest=hmac-sha512', '--auth=sjk3kqzcSV3XfHJWNstn']
-        )
 
-    def test_hmac_crc32(self):
-        self.run_listener_client(
-            ['--nagios=1000,5%,1000,5%', '--count=1', '--auth-digest=hmac-crc32', '--auth=mc82kJwtXFlhqQSCKptQ'],
-            ['--auth-digest=hmac-crc32', '--auth=mc82kJwtXFlhqQSCKptQ']
-        )
+class TestCLIHMACSHA1(BaseTestCLI):
+    listener_opts = ['--auth-digest=hmac-sha1', '--auth=qnzTCJHnZXdrxRZ8JjQw']
+
+    def test_hmac(self):
+        self.run_listener_client(self.listener_opts)
+
+
+class TestCLIHMACSHA256(BaseTestCLI):
+    listener_opts = ['--auth-digest=hmac-sha256', '--auth=cc8G2Ssbq4WZRq7H7d5L']
+
+    def test_hmac(self):
+        self.run_listener_client(self.listener_opts)
+
+
+class TestCLIHMACSHA512(BaseTestCLI):
+    listener_opts = ['--auth-digest=hmac-sha512', '--auth=sjk3kqzcSV3XfHJWNstn']
+
+    def test_hmac(self):
+        self.run_listener_client(self.listener_opts)
+
+
+class TestCLIHMACCRC32(BaseTestCLI):
+    listener_opts = ['--auth-digest=hmac-crc32', '--auth=mc82kJwtXFlhqQSCKptQ']
+
+    def test_hmac(self):
+        self.run_listener_client(self.listener_opts)
 
 
 if __name__ == '__main__':
