92cb776b0a0d60da979278073c89d96af5577062
Ryan Finnie <ryan@finnie.org>
2020-06-08 21:56:23 -0700

    monotonic() can be equal if within rounding time / resolution

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,4 +6,4 @@ class Test2Ping(unittest.TestCase):
     def test_monotonic(self):
         val1 = time.monotonic()
         val2 = time.monotonic()
-        self.assertGreater(val2, val1)
+        self.assertGreaterEqual(val2, val1)
